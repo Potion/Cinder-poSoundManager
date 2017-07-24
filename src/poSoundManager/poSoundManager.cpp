e0b3ba4d227666630c652fe35ac6dce39c59afd2
peterobbin <beijingwang@gmail.com>
2017-07-24 15:51:32 -0400

    added function to set group gain

--- src/poSoundManager/poSoundManager.cpp
+++ src/poSoundManager/poSoundManager.cpp
@@ -1,21 +1,21 @@
 /*
  Copyright (c) 2015, Potion Design LLC
  All rights reserved.
- 
+
  Redistribution and use in source and binary forms, with or without
  modification, are permitted provided that the following conditions are met:
- 
+
  * Redistributions of source code must retain the above copyright notice, this
  list of conditions and the following disclaimer.
- 
+
  * Redistributions in binary form must reproduce the above copyright notice,
  this list of conditions and the following disclaimer in the documentation
  and/or other materials provided with the distribution.
- 
+
  * Neither the name of copyright holder nor the names of its
  contributors may be used to endorse or promote products derived from
  this software without specific prior written permission.
- 
+
  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -32,216 +32,232 @@
 #include "cinder/Rand.h"
 #include "cinder/app/App.h"
 
-namespace po {
-	
+namespace po
+{
+
 	SoundManagerRef SoundManager::Instance = nullptr;
-	
+
 	SoundManagerRef SoundManager::get()
 	{
-		if (!Instance) {
-			Instance = std::shared_ptr<SoundManager>(new SoundManager());
+		if( !Instance ) {
+			Instance = std::shared_ptr<SoundManager>( new SoundManager() );
 			Instance->setup();
 		}
-		
+
 		return Instance;
 	}
-	
+
 	SoundManager::SoundManager()
-	: mTrackID(0)
-	, mAverageVolume(0.f)
-    , mSilentMode(false)
+		: mTrackID( 0 )
+		, mAverageVolume( 0.f )
+		, mSilentMode( false )
 	{}
-	
+
 	void SoundManager::setup()
 	{
 		auto context = ci::audio::Context::master();
-        mMasterGain = context->makeNode(new ci::audio::GainNode(1.0));
-        context->enable();
+		mMasterGain = context->makeNode( new ci::audio::GainNode( 1.0 ) );
+		context->enable();
 	}
-    
-    
-    void SoundManager::setSilentMode(bool silent)
-    {
-        mSilentMode = silent;
-        
-        float gain = mSilentMode ? 0.f : 1.f;
-        mMasterGain->setValue(gain);
-    }
-	
-    
+
+
+	void SoundManager::setSilentMode( bool silent )
+	{
+		mSilentMode = silent;
+
+		float gain = mSilentMode ? 0.f : 1.f;
+		mMasterGain->setValue( gain );
+	}
+
+
 	void SoundManager::update()
 	{
 		// clear out tracks that are done playing
 		int numTracksPlaying = 0;
-        
-		for (auto thisTrack = mTracks.begin(); thisTrack != mTracks.end();) {
-            TrackRef t = thisTrack->second;
-		
+
+		for( auto thisTrack = mTracks.begin(); thisTrack != mTracks.end(); ) {
+			TrackRef t = thisTrack->second;
+
 			bool bRemove = true;
-			if ( t!= nullptr && !t->isFinished() )  {
+
+			if( t != nullptr && !t->isFinished() )  {
 				numTracksPlaying++;
 				++thisTrack;
 				bRemove = false;
 			}
-//			} else if( t!= nullptr && t->isFinished() ){
-//				bRemove = true;
-//			}
-			
+
+			//			} else if( t!= nullptr && t->isFinished() ){
+			//				bRemove = true;
+			//			}
+
 			if( bRemove ) {
 				//  Remove reference to track
-				onFinishedPlaying(thisTrack->first);
+				onFinishedPlaying( thisTrack->first );
 				t->disconnect();
 				//ci::app::console() << "removing track \n";
-				mGroup.erase(thisTrack->first);
-				thisTrack = mTracks.erase(thisTrack);
+				mGroup.erase( thisTrack->first );
+				thisTrack = mTracks.erase( thisTrack );
 			}
 		}
-		
+
 		// adjust gain
 		mAverageVolume = mSilentMode ? 0 : MAX_VOLUME / numTracksPlaying;
 
-		for (auto thisTrack = mTracks.begin(); thisTrack != mTracks.end(); ++thisTrack) {
+		for( auto thisTrack = mTracks.begin(); thisTrack != mTracks.end(); ++thisTrack ) {
 			TrackRef t = thisTrack->second;
-            t->gain->getParam()->applyRamp(mAverageVolume, RAMP_TIME, ci::audio::Param::Options().rampFn(&ci::audio::rampInQuad));
+			t->gain->getParam()->applyRamp( mAverageVolume, RAMP_TIME, ci::audio::Param::Options().rampFn( &ci::audio::rampInQuad ) );
 		}
-		
+
 	}
-	
-    
-    unsigned int SoundManager::play(ci::DataSourceRef ref, unsigned int group, bool loop)
+
+
+	unsigned int SoundManager::play( ci::DataSourceRef ref, unsigned int group, bool loop )
 	{
-        ci::audio::SourceFileRef sourceFile = ci::audio::SourceFile::create(ref);
-        ci::audio::BufferRef buffer = sourceFile->loadBuffer();
-		return play(buffer, group, loop);
+		ci::audio::SourceFileRef sourceFile = ci::audio::SourceFile::create( ref );
+		ci::audio::BufferRef buffer = sourceFile->loadBuffer();
+		return play( buffer, group, loop );
 	}
-	
-    
-    unsigned int SoundManager::play(ci::audio::BufferRef buffer, unsigned int group, bool loop)
-    {
-        auto context = ci::audio::Context::master();
-        
-        //  Create Buffer Player
-		ci::audio::BufferPlayerNodeRef bufferPlayer = context->makeNode(new ci::audio::BufferPlayerNode(buffer));
-		bufferPlayer->setLoopEnabled(loop);
-		
-        
-        //  Create Track
-        TrackRef t(new Track(bufferPlayer));
-		
-        //  Connect track w/Master Gain Node
-        t->connect(mMasterGain);
-        
-        //Save track
-        mGroup[mTrackID]    = group;
-        mTracks[mTrackID]   = t;
-        
-        //  Start track
-        t->bufferPlayer->start();
-		
-        //  Increase track count and return prev id
-        unsigned int currentTrackID = mTrackID;
-        mTrackID++;
-        return currentTrackID;
-    }
-	
-	void SoundManager::play(unsigned int trackID,bool bLoop )
+
+
+	unsigned int SoundManager::play( ci::audio::BufferRef buffer, unsigned int group, bool loop )
 	{
-		if(trackExists(trackID)) {
-			mTracks[trackID]->bufferPlayer->setLoopEnabled(bLoop);
+		auto context = ci::audio::Context::master();
+
+		//  Create Buffer Player
+		ci::audio::BufferPlayerNodeRef bufferPlayer = context->makeNode( new ci::audio::BufferPlayerNode( buffer ) );
+		bufferPlayer->setLoopEnabled( loop );
+
+
+		//  Create Track
+		TrackRef t( new Track( bufferPlayer ) );
+
+		//  Connect track w/Master Gain Node
+		t->connect( mMasterGain );
+
+		//Save track
+		mGroup[mTrackID]    = group;
+		mTracks[mTrackID]   = t;
+
+		//  Start track
+		t->bufferPlayer->start();
+
+		//  Increase track count and return prev id
+		unsigned int currentTrackID = mTrackID;
+		mTrackID++;
+		return currentTrackID;
+	}
+
+	void SoundManager::play( unsigned int trackID, bool bLoop )
+	{
+		if( trackExists( trackID ) ) {
+			mTracks[trackID]->bufferPlayer->setLoopEnabled( bLoop );
 			mTracks[trackID]->bufferPlayer->start();
 		}
 	}
-	
-	void SoundManager::stop(unsigned int trackID)
+
+	void SoundManager::stop( unsigned int trackID )
 	{
-        if(trackExists(trackID)) {
-            mTracks[trackID]->bufferPlayer->stop();
-            mTracks[trackID]->bufferPlayer->setLoopEnabled(false);
-        }
-	}
-    
-    
-    void SoundManager::stopAllInGroup(unsigned int group)
-    {
-        for (auto thisGroup = mGroup.begin(); thisGroup != mGroup.end(); ++thisGroup) {
-            unsigned int g = thisGroup->second;
-            if (g == group) {
-                unsigned int trackID = thisGroup->first;
-                stop(trackID);
-            }
-        }
-    }
-	
-    
-	void SoundManager::stopAll()
-	{
-		for (auto thisTrack = mTracks.begin(); thisTrack != mTracks.end(); ++thisTrack) {
-            thisTrack->second->bufferPlayer->stop();
+		if( trackExists( trackID ) ) {
+			mTracks[trackID]->bufferPlayer->stop();
+			mTracks[trackID]->bufferPlayer->setLoopEnabled( false );
 		}
 	}
-	
+
+
+	void SoundManager::stopAllInGroup( unsigned int group )
+	{
+		for( auto thisGroup = mGroup.begin(); thisGroup != mGroup.end(); ++thisGroup ) {
+			unsigned int g = thisGroup->second;
+
+			if( g == group ) {
+				unsigned int trackID = thisGroup->first;
+				stop( trackID );
+			}
+		}
+	}
+
+
+	void SoundManager::stopAll()
+	{
+		for( auto thisTrack = mTracks.begin(); thisTrack != mTracks.end(); ++thisTrack ) {
+			thisTrack->second->bufferPlayer->stop();
+		}
+	}
+
 	void SoundManager::removeAllTracks()
 	{
 		stopAll();
-		
-		for (auto thisTrack = mTracks.begin(); thisTrack != mTracks.end();) {
+
+		for( auto thisTrack = mTracks.begin(); thisTrack != mTracks.end(); ) {
 			//  Remove reference to track
 			TrackRef t = thisTrack->second;
-			onFinishedPlaying(thisTrack->first);
+			onFinishedPlaying( thisTrack->first );
 			t->disconnect();
-			mGroup.erase(thisTrack->first);
-			thisTrack = mTracks.erase(thisTrack);
+			mGroup.erase( thisTrack->first );
+			thisTrack = mTracks.erase( thisTrack );
 		}
-		
+
 		mGroup.empty();
 		mTracks.empty();
 	}
-	
-    
-    //  Has the track completed?
-    bool SoundManager::isSoundFinishedPlaying(unsigned int trackID)
-    {
-        return !trackExists(trackID);
-    }
-    
-    
-    //  Check whether or not we know about the track still
-    bool SoundManager::trackExists(unsigned int trackID)
-    {
-        bool exists = (mTracks.find(trackID) != mTracks.end());
-        
-        if(exists) {
-            return mTracks[trackID] != nullptr;
-        }
-        
-        return false;
-    }
-	
-    
+
+
+	//  Has the track completed?
+	bool SoundManager::isSoundFinishedPlaying( unsigned int trackID )
+	{
+		return !trackExists( trackID );
+	}
+
+
+	//  Check whether or not we know about the track still
+	bool SoundManager::trackExists( unsigned int trackID )
+	{
+		bool exists = ( mTracks.find( trackID ) != mTracks.end() );
+
+		if( exists ) {
+			return mTracks[trackID] != nullptr;
+		}
+
+		return false;
+	}
+
+
 	//
 	//	Send a message when a track has finished playing
 	//
-	void SoundManager::onFinishedPlaying(unsigned int trackID)
+	void SoundManager::onFinishedPlaying( unsigned int trackID )
 	{
 		//ci::app::console() << "SoundManager::onFinishedPlaying:: " << trackID << std::endl;
-		mSoundFinishedPlaying.emit(trackID);
+		mSoundFinishedPlaying.emit( trackID );
 	}
-	
+
 	//
 	//	Set the track gain, ramps volume up and down
 	//
-	void SoundManager::setGain(unsigned int trackID, float volume)
+	void SoundManager::setGain( unsigned int trackID, float volume )
 	{
-		if (mTracks.find(trackID) != mTracks.end()) {
-			mTracks[trackID]->gain->getParam()->applyRamp(volume, RAMP_TIME, ci::audio::Param::Options().rampFn(&ci::audio::rampInQuad));
+		if( mTracks.find( trackID ) != mTracks.end() ) {
+			mTracks[trackID]->gain->getParam()->applyRamp( volume, RAMP_TIME, ci::audio::Param::Options().rampFn( &ci::audio::rampInQuad ) );
 		}
 	}
-	
-	void SoundManager::setPan(unsigned int trackID, float pan)
+
+	void SoundManager::setGroupGain( unsigned int groupID, float volume )
 	{
-		if (mTracks.find(trackID) != mTracks.end()) {
-			mTracks[trackID]->pan->setPos(pan);
+		for( auto thisGroup = mGroup.begin(); thisGroup != mGroup.end(); ++thisGroup ) {
+			unsigned int g = thisGroup->second;
+
+			if( g == groupID ) {
+				unsigned int trackID = thisGroup->first;
+				setGain( trackID, volume );
+			}
 		}
 	}
-	
+
+	void SoundManager::setPan( unsigned int trackID, float pan )
+	{
+		if( mTracks.find( trackID ) != mTracks.end() ) {
+			mTracks[trackID]->pan->setPos( pan );
+		}
+	}
+
 }
--- src/poSoundManager/poSoundManager.h
+++ src/poSoundManager/poSoundManager.h
@@ -1,21 +1,21 @@
 /*
  Copyright (c) 2015, Potion Design LLC
  All rights reserved.
- 
+
  Redistribution and use in source and binary forms, with or without
  modification, are permitted provided that the following conditions are met:
- 
+
  * Redistributions of source code must retain the above copyright notice, this
  list of conditions and the following disclaimer.
- 
+
  * Redistributions in binary form must reproduce the above copyright notice,
  this list of conditions and the following disclaimer in the documentation
  and/or other materials provided with the distribution.
- 
+
  * Neither the name of copyright holder nor the names of its
  contributors may be used to endorse or promote products derived from
  this software without specific prior written permission.
- 
+
  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -37,118 +37,125 @@
 #include "cinder/audio/PanNode.h"
 #include "cinder/audio/SamplePlayerNode.h"
 
-namespace po {
+namespace po
+{
 	class SoundManager;
 	typedef std::shared_ptr<SoundManager> SoundManagerRef;
-	
+
 	class SoundManager
 	{
-	public:
-		static SoundManagerRef get();
-		
-		//	Sound finished playing signal
-		typedef ci::signals::Signal<void(unsigned int)> SoundFinishedPlayingSignal;
-		
-		void update();
-		
-		//	Play data source
-        unsigned int play(ci::DataSourceRef dataSource, unsigned int group=0, bool loop = false);
-		
-		//	Play audio buffer
-        unsigned int play(ci::audio::BufferRef buffer, unsigned int group=0, bool loop = false);
-		
-		// play existing track
-		void play( unsigned int trackID, bool bLoop = false );
-		
-		//	Stop
-		void stop(unsigned int trackID);
-		
-		//	Stop all in specific group
-        void stopAllInGroup(unsigned int group);
-		
-		//	Stop all sounds
-		void stopAll();
-		
-		//	Remove all tracks
-		void removeAllTracks();
-		
-		//	Silence the sounds
-        void setSilentMode(bool silent);
-		
-		//	Track gain
-		void setGain(unsigned int trackID, float volume);
-		
-		//	Track panning
-		void setPan(unsigned int trackID, float pan);
-		
-		//	Check if a track finished playing
-        bool isSoundFinishedPlaying(unsigned int trackID);
-		SoundFinishedPlayingSignal &getSignalSoundFinishedPlaying() { return mSoundFinishedPlaying; }
-		
-	protected:
-		SoundManager();
-		
-		void setup();
-		
-	private:
-		//	Track data
-        struct Track {
-            ci::audio::BufferPlayerNodeRef bufferPlayer;
-            ci::audio::GainNodeRef gain;
-            ci::audio::MonitorNodeRef monitor;
-            ci::audio::Pan2dNodeRef pan;
-			
-            Track(ci::audio::BufferPlayerNodeRef bufferPlayer)
-            : bufferPlayer(bufferPlayer)
-            {
-                auto context = ci::audio::Context::master();
-                
-                gain    = context->makeNode(new ci::audio::GainNode(1.0));
-                monitor = context->makeNode(new ci::audio::MonitorNode);
-				pan     = context->makeNode(new ci::audio::Pan2dNode());
-				pan->setStereoInputModeEnabled();
-            }
-            
-            bool isFinished() {
-                return (!bufferPlayer->isEnabled() && !bufferPlayer->isLoopEnabled());
-            }
-            
-            void connect(ci::audio::GainNodeRef masterGain) {
-                auto context = ci::audio::Context::master();
-                bufferPlayer >> monitor >> gain >> pan >> masterGain >> context->getOutput();
-            }
-            
-            void disconnect() {
-                gain->disconnectAll();
-                monitor->disconnectAll();
-                pan->disconnectAll();
-                bufferPlayer->disconnectAll();
-            }
-        };
-        
-        typedef std::shared_ptr<Track> TrackRef;
-        
-		static SoundManagerRef Instance;
-		
-		float MAX_VOLUME = 0.9f;
-		const float RAMP_TIME = 0.1f;
-		const float PAN_CENTER = 0.5f;
-		
-		unsigned int mTrackID;
-        
-        bool trackExists(unsigned int trackID);
-        std::map<unsigned int, unsigned int> mGroup;
-		std::map<unsigned int, TrackRef> mTracks;
-        
-		float mAverageVolume;
-		ci::audio::GainNodeRef mMasterGain;
-        
-        bool mSilentMode;
-		
-		void onFinishedPlaying(unsigned int trackID);
-	
-		SoundFinishedPlayingSignal mSoundFinishedPlaying;
-		
+		public:
+			static SoundManagerRef get();
+
+			//	Sound finished playing signal
+			typedef ci::signals::Signal<void( unsigned int )> SoundFinishedPlayingSignal;
+
+			void update();
+
+			//	Play data source
+			unsigned int play( ci::DataSourceRef dataSource, unsigned int group = 0, bool loop = false );
+
+			//	Play audio buffer
+			unsigned int play( ci::audio::BufferRef buffer, unsigned int group = 0, bool loop = false );
+
+			// play existing track
+			void play( unsigned int trackID, bool bLoop = false );
+
+			//	Stop
+			void stop( unsigned int trackID );
+
+			//	Stop all in specific group
+			void stopAllInGroup( unsigned int group );
+
+			//	Stop all sounds
+			void stopAll();
+
+			//	Remove all tracks
+			void removeAllTracks();
+
+			//	Silence the sounds
+			void setSilentMode( bool silent );
+
+			//	Track gain
+			void setGain( unsigned int trackID, float volume );
+
+			//  Group gain
+			void setGroupGain( unsigned int groupID, float volume );
+
+			//	Track panning
+			void setPan( unsigned int trackID, float pan );
+
+			//	Check if a track finished playing
+			bool isSoundFinishedPlaying( unsigned int trackID );
+			SoundFinishedPlayingSignal& getSignalSoundFinishedPlaying() { return mSoundFinishedPlaying; }
+
+		protected:
+			SoundManager();
+
+			void setup();
+
+		private:
+			//	Track data
+			struct Track {
+				ci::audio::BufferPlayerNodeRef bufferPlayer;
+				ci::audio::GainNodeRef gain;
+				ci::audio::MonitorNodeRef monitor;
+				ci::audio::Pan2dNodeRef pan;
+
+				Track( ci::audio::BufferPlayerNodeRef bufferPlayer )
+					: bufferPlayer( bufferPlayer )
+				{
+					auto context = ci::audio::Context::master();
+
+					gain    = context->makeNode( new ci::audio::GainNode( 1.0 ) );
+					monitor = context->makeNode( new ci::audio::MonitorNode );
+					pan     = context->makeNode( new ci::audio::Pan2dNode() );
+					pan->setStereoInputModeEnabled();
+				}
+
+				bool isFinished()
+				{
+					return ( !bufferPlayer->isEnabled() && !bufferPlayer->isLoopEnabled() );
+				}
+
+				void connect( ci::audio::GainNodeRef masterGain )
+				{
+					auto context = ci::audio::Context::master();
+					bufferPlayer >> monitor >> gain >> pan >> masterGain >> context->getOutput();
+				}
+
+				void disconnect()
+				{
+					gain->disconnectAll();
+					monitor->disconnectAll();
+					pan->disconnectAll();
+					bufferPlayer->disconnectAll();
+				}
+			};
+
+			typedef std::shared_ptr<Track> TrackRef;
+
+			static SoundManagerRef Instance;
+
+			float MAX_VOLUME = 0.9f;
+			const float RAMP_TIME = 0.1f;
+			const float PAN_CENTER = 0.5f;
+
+			unsigned int mTrackID;
+
+			bool trackExists( unsigned int trackID );
+			std::map<unsigned int, unsigned int> mGroup;
+			std::map<unsigned int, TrackRef> mTracks;
+
+			float mAverageVolume;
+			ci::audio::GainNodeRef mMasterGain;
+
+			bool mSilentMode;
+
+			void onFinishedPlaying( unsigned int trackID );
+
+			SoundFinishedPlayingSignal mSoundFinishedPlaying;
+
 	};
-	
+
 }
